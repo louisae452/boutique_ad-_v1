7376bd5c47b529470b33d74ca43b2dbf2c12a975
louisae452 <louisae452@msn.com>
2026-07-22 10:37:08 +0100

    Add sorting

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, reverse, redirect
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
@@ -10,13 +11,27 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+        current_sorting = f'{sort}_{direction}'
+        
+        
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = Product.objects.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-        
-        
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,13 +39,14 @@ def all_products(request):
                 return redirect(reverse('products'))
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-                
+
     context = {
         'products': products,
         'search_tem': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
-    
+
     return render(request, 'products/products.html', context)
 
 
@@ -40,5 +56,5 @@ def product_detail(request, product_id):
     context = {
         'product': product,
     }
-    
+
     return render(request, 'products/product_detail.html', context)
